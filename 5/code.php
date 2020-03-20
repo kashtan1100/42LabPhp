835bc4623ca96e39b64929e7e8b91d3da8503096
Kashtan <otabekxama822@gmail.com>
2020-03-20 03:45:49 +0300

    5

---
+++ 5/code.php
@@ -0,0 +1,58 @@
+<?php
+
+//function regex_check($password){
+//    $reg = "(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}";
+//    if(!preg_match($reg, $password)){
+//        echo 'Капец';
+//    }
+//}
+
+function regex($password)
+{
+    $reg_one = "/^\S{10,}$/";
+    if (!preg_match($reg_one, $password)) {
+        echo 'Меньше чем 10</br>';
+    }
+
+    $reg_two_num = "/(\S*\d+\S*){2,}/";
+    if (!preg_match($reg_two_num, $password)) {
+        echo 'Строка не содержит по крайней мере 2 чисел </br>';
+    }
+
+    $reg_two_uppercase = "/^(.*?[A-Z]){2,}.*$/";
+    if (!preg_match($reg_two_uppercase, $password)) {
+        echo 'Строка не содержит по крайней мере 2 прописных буквы</br>';
+    }
+
+    $reg_two_lowercase = "/^(.*?[a-z]){2,}.*$/";
+    if (!preg_match($reg_two_lowercase, $password)) {
+        echo 'Строка не содержит по крайней мере 2 строчных буквы</br>';
+    }
+
+    $reg_two_spec = "/^(.*?[%$#_*]){2,}.*$/";
+    if (!preg_match($reg_two_spec, $password)) {
+        echo 'Строка не содержит не менее 2 %$#_*</br>';
+    }
+
+    $reg_three_uppercase_contract = "/[A-Z]{3,}/";
+    if(preg_match($reg_three_uppercase_contract, $password)){
+        echo 'Пароль содержит более 3 заглавных букв подряд</br>';
+    }
+
+    $reg_three_lowercase_contract = "/\d{3,}/";
+    if(preg_match($reg_three_lowercase_contract, $password)){
+        echo 'Пароль содержит более 3 цифр подряд</br>';
+    }
+
+    $reg_three_spec_contract = "/\d{3,}/";
+    if(preg_match($reg_three_spec_contract, $password)){
+        echo 'пароль содержит более 3 спецсимволов подряд</br>';
+    }
+}
+
+
+if (isset($_REQUEST['password'])) {
+    regex($_REQUEST['password']);
+} else {
+    include 'index.html';
+}
