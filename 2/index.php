efe5d4e7b4106dbcabb9cb86c96c644e0a21fb86
Kashtan <otabekxama822@gmail.com>
2020-04-14 15:17:52 +0300

    add generator yield

--- 2/index.php
+++ 2/index.php
@@ -11,47 +11,49 @@
 
 function replace_letter(string $str)
 {
-    $strResult = '';
-    $counter =0;
+    //$strResult = '';
+    $counter = 0;
     for ($i = 0; $i < strlen($str); $i++) {
         switch ($str[$i]) {
             case 'h' :
-                $replaced_char = '4';
                 $counter++;
+                yield '4';
                 break;
             case 'i' :
-                $replaced_char = '1';
                 $counter++;
+                yield '1';
                 break;
             case 'e' :
-                $replaced_char = '3';
                 $counter++;
+                yield '3';
                 break;
             case 'o' :
-                $replaced_char = '0';
                 $counter++;
+                yield '0';
                 break;
             default :
-                $replaced_char = $str[$i];
+                yield $str[$i];
         }
-        $strResult .= $replaced_char;
     }
-    if($strResult == '42')
-        $strResult = 'WHAT DO YOU GET IF YOU MULTIPLY SIX BY NINE? <br> '.$strResult;
-    $strResult.="<br> Число замен ".$counter;
-    return $strResult;
+    echo "<br>"."Изменений ".$counter;
+}
+
+function replace_change($str){
+    foreach (replace_letter($str) as $value){
+        echo $value;
+    }
 }
 
 if (isset($_REQUEST['string'])) {
-    $req = replace_letter($_REQUEST['string']);
-    echo $req;
-}
-else{
+    //$req = replace_letter($_REQUEST['string']);
+    $req = $_REQUEST['string'];
+    replace_change($req);
+} else {
     echo '<form action="index.php">
     string : <input type="text" name="string" value="">
     <input type="submit" value="Отправить">
 </form>';
 
-}?>
+} ?>
 </body>
 </html>
